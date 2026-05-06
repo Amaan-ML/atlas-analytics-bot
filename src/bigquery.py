42610963b34e42f3381238385f53da2e03500b7c
Amaan Mohammad <amaan@mavenlane.com>
2026-05-06 23:14:42 +0530

    Update bigquery.py for Render deployment

--- src/bigquery.py
+++ src/bigquery.py
@@ -1,7 +1,17 @@
 from google.cloud.bigquery import Client
 import os
+import json
+import tempfile
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(os.path.dirname(__file__), '..', 'bq-service-account.json')
+credentials_path = os.path.join(os.path.dirname(__file__), '..', 'bq-service-account.json')
+
+if os.path.exists(credentials_path):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+elif os.environ.get("GOOGLE_CREDENTIALS_JSON"):
+    creds = json.loads(os.environ["GOOGLE_CREDENTIALS_JSON"])
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.json', delete=False) as f:
+        json.dump(creds, f)
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = f.name
 
 client = Client()
 
